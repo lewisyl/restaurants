c7ef2b4ddce4c2163eae92d5dfe809e94e5ca79d
Lewis Liu <lewisliuusa@outlook.com>
2020-03-03 22:10:18 -0800

    asd

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -8,10 +8,10 @@ import { HttpService } from "./Services/http-service/http.service";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 
-import { NewRestaurantComponent } from "./new-restaurant/new-restaurant.component";
-import { ShowRestaurantsComponent } from "./show-restaurants/show-restaurants.component";
-import { ShowRatesComponent } from "./show-rates/show-rates.component";
-import { NewRateComponent } from "./new-rate/new-rate.component";
+import { NewRestaurantComponent } from './new-restaurant/new-restaurant.component';
+import { ShowRestaurantsComponent } from './show-restaurants/show-restaurants.component';
+import { ShowRatesComponent } from './show-rates/show-rates.component';
+import { NewRateComponent } from './new-rate/new-rate.component';
 import { EditRestaurantComponent } from "./edit-restaurant/edit-restaurant.component";
 
 @NgModule({
